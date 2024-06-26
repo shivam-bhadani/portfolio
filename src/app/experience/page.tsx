670e296c22db0614b3926148c5b5278869f5bacb
shivam-bhadani <shivambhadani123@gmail.com>
2024-06-26 23:17:57 +0530

    Fixed margin in experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -11,8 +11,8 @@ const Experience = () => {
             key={`exp_${key_exp}`}
             duration={Math.floor(Math.random() * 10000) + 10000}
             borderRadius="1.5rem"
-            className="bg-white dark:bg-dark-color-2 text-black dark:text-white border-neutral-200 dark:border-slate-800 p-10 flex flex-col mt-4 md:mt-0"
-            containerClassName="m-auto w-[900px] max-w-[90%] h-auto md:mt-10"
+            className="bg-white dark:bg-dark-color-2 text-black dark:text-white border-neutral-200 dark:border-slate-800 p-10 flex flex-col"
+            containerClassName="m-auto w-[900px] max-w-[90%] h-auto mt-4 md:mt-10"
           >
             <div className="w-[100%] md:px-6 px-1">
               <div className="flex justify-between">
