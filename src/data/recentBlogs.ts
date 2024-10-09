ec941414de996a1660ae8e0fb68966d5994ba059
shivam-bhadani <shivambhadani123@gmail.com>
2024-10-09 16:18:00 +0530

    added new blog

--- src/data/recentBlogs.ts
+++ src/data/recentBlogs.ts
@@ -2,8 +2,14 @@ import { IRecentBlog } from "./types";
 import introduction_to_cloud from "@/assets/blogImages/introduction_to_cloud.png"
 import ebs from "@/assets/blogImages/ebs.png"
 import horizontal_scaling_load_balancer from "@/assets/blogImages/horizontal_scaling_load_balancer.png"
+import tcp from "@/assets/blogImages/tcp.png"
 
 const recentBlogs : Array<IRecentBlog> = [
+  {
+    title: "Understanding TCP and building our own TCP Server in C language",
+    thumbnail: tcp,
+    link: "https://medium.com/@shivambhadani_/understanding-tcp-and-building-our-own-tcp-server-in-c-language-8de9d9de78ef"
+  },
   {
     title: "Introduction to Cloud and EC2",
     thumbnail: introduction_to_cloud,
