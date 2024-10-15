61c47adae3b13f41b8b948d4a4a8fe6068095a63
shivam-bhadani <shivambhadani123@gmail.com>
2024-10-15 19:57:33 +0530

    added new blog

--- src/data/recentBlogs.ts
+++ src/data/recentBlogs.ts
@@ -3,8 +3,14 @@ import introduction_to_cloud from "@/assets/blogImages/introduction_to_cloud.png
 import ebs from "@/assets/blogImages/ebs.png"
 import horizontal_scaling_load_balancer from "@/assets/blogImages/horizontal_scaling_load_balancer.png"
 import tcp from "@/assets/blogImages/tcp.png"
+import asg from "@/assets/blogImages/asg.png"
 
 const recentBlogs : Array<IRecentBlog> = [
+  {
+    title: "Step-by-Step Guide to Create Your First AWS Auto Scaling Group",
+    thumbnail: asg,
+    link: "https://medium.com/@shivambhadani_/aws-part-4-step-by-step-guide-to-create-your-first-aws-auto-scaling-group-826c1effc0ec"
+  },
   {
     title: "Understanding TCP and building our own TCP Server in C language",
     thumbnail: tcp,
