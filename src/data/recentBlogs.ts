0fea2c49c2539013a8ab6245164b26b4e34b8f50
shivam-bhadani <shivambhadani123@gmail.com>
2024-11-06 15:02:04 +0530

    added new blog

--- src/data/recentBlogs.ts
+++ src/data/recentBlogs.ts
@@ -1,16 +1,21 @@
 import { IRecentBlog } from "./types";
 import introduction_to_cloud from "@/assets/blogImages/introduction_to_cloud.png"
-import ebs from "@/assets/blogImages/ebs.png"
 import horizontal_scaling_load_balancer from "@/assets/blogImages/horizontal_scaling_load_balancer.png"
 import tcp from "@/assets/blogImages/tcp.png"
 import asg from "@/assets/blogImages/asg.png"
 import vpc from "@/assets/blogImages/vpc.png"
+import rpc from "@/assets/blogImages/rpc.png"
 
 const recentBlogs : Array<IRecentBlog> = [
   {
     title: "Learn Complete AWS VPC in Just One Article",
     thumbnail: vpc,
     link: "https://medium.com/@shivambhadani_/aws-part-5-learn-complete-aws-vpc-in-just-one-article-5ffe34888a5c"
+  },
+  {
+    title: "Introduction to RPC in Go: Building RPC client and server with Golang",
+    thumbnail: rpc,
+    link: "https://medium.com/@shivambhadani_/introduction-to-rpc-in-go-building-rpc-client-and-server-with-golang-5794675e9a12"
   },
   {
     title: "Step-by-Step Guide to Create Your First AWS Auto Scaling Group",
@@ -26,11 +31,6 @@ const recentBlogs : Array<IRecentBlog> = [
     title: "Introduction to Cloud and EC2",
     thumbnail: introduction_to_cloud,
     link: "https://medium.com/@shivambhadani_/aws-part-1-introduction-to-cloud-and-ec2-f06cdc80a1fc"
-  },
-  {
-    title: "A Beginner's Guide to Understanding AWS EBS Volumes",
-    thumbnail: ebs,
-    link: "https://medium.com/@shivambhadani_/aws-part-2-a-beginners-guide-to-understanding-aws-ebs-volumes-a6f87b1140c8"
   },
   {
     title: "Horizontal Scaling and Load Balancer",
