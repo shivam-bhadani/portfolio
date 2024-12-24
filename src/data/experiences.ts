3b8cff571e1af897c2b44a9c99f4e5a26cf97bc6
shivam-bhadani <shivambhadani123@gmail.com>
2024-12-24 14:56:35 +0530

    Added experience

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -2,6 +2,18 @@ import { IExperience } from "./types";
 
 const experiences : Array<IExperience> = [
   {
+    company: "Nuvama Wealth",
+    role: "Software Developer",
+    work: [
+      "Developed robust Spring Boot APIs for PnL, Holding, and Insurance reports in a fintech trading environment",
+      "Automated report generation in Excel using Apache POI and in PDF format using OpenPDF and iText",
+      "Employed usage of MongoDB PostgreSQL for efficient storage of persistent data and Redis for caching",
+      "Built frontend using ReactJS in clean-architecture, ensuring modularity and maintainability in the application"
+    ],
+    joining_date: "June 2024",
+    ending_date: "December 2024"
+  },
+  {
     company: "Airblack",
     role: "Fullstack Intern",
     work: [
